5347b6340253c696ce95e7f32e1bc9eca83d433e
alincmt <aclimente@ymail.com>
2019-08-02 14:38:20 +0300

    Add files via upload

---
+++ read_local_csv_file/script.js
@@ -0,0 +1,89 @@
+
+document.getElementById('txtFileUpload').addEventListener('change', upload, false);
+
+ function upload(evt) {
+
+ var data = null;
+ var file = evt.target.files[0];
+ var reader = new FileReader();
+ reader.readAsText(file);
+ reader.onload = function(event) {
+ var csvData = event.target.result;
+
+ var data = Papa.parse(csvData, {header : true});
+
+ console.log(data);
+
+ };
+ reader.onerror = function() {
+ alert('Unable to read ' + file.fileName);
+ };
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if (inputType == "local")
+// 		{
+// 			if (!$('#files')[0].files.length)
+// 			{
+// 				alert("Please choose at least one file to parse.");
+// 				return enableButton();
+// 			}
+//
+// 			$('#files').parse({
+// 				config: config,
+// 				before: function(file, inputElem)
+// 				{
+// 					start = now();
+// 					console.log("Parsing file...", file);
+// 				},
+// 				error: function(err, file)
+// 				{
+// 					console.log("ERROR:", err, file);
+// 					firstError = firstError || err;
+// 					errorCount++;
+// 				},
+// 				complete: function()
+// 				{
+// 					end = now();
+// 					printStats("Done with all files");
+// 				}
+// 			});
+
+//
+// <script src="./papaparse.min.js"></script>
+// <script src="./jquery-2.1.1.min.js"></script>
+// <script>
+//   var data;
+//
+//   function handleFileSelect(evt) {
+//     var file = evt.target.files[0];
+//
+//     Papa.parse(file, {
+//       header: true,
+//       dynamicTyping: true,
+//       complete: function(results) {
+//         data = results;
+//       }
+//     });
+//   }
+//
+//   $(document).ready(function(){
+//     $("#csv-file").change(handleFileSelect);
+//   });
+// </script>
+// <input type="file" id="csv-file" name="files"/>
